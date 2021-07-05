cd6812325ed47a5e30597bdf9fba42f248d2dde7
Prakhar Shukla <imprakharshukla@gmail.com>
2021-07-05 09:03:16 +0530

    added modded os images into /static/images/moddedos

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,12 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   docs: {
     primaryColor: '#FF8B25'
-  }
+  },
+  link:[
+    {
+      rel: 'icon',
+      type:'image/png',
+      href:'/icon2.png?v3'
+    }
+  ]
 })
